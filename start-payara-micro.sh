50ee46dde9e37abb80099af47f2e42f44d9cea5f
Gonzalo <gonzalogutierrezga@gmail.com>
2026-02-25 12:17:10 +0100

    fix: remove colon from JNDI resource name

--- start-payara-micro.sh
+++ start-payara-micro.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -eu
 
+echo "=== Generating post-boot commands ==="
+
 cat > /opt/payara/config/post-boot-generated.asadmin <<EOF
 create-jdbc-connection-pool --datasourceclassname=org.postgresql.ds.PGSimpleDataSource --restype=javax.sql.DataSource --property user=${DB_USER}:password=${DB_PASSWORD}:serverName=${DB_HOST}:portNumber=${DB_PORT}:databaseName=${DB_NAME}:sslmode=${DB_SSL_MODE} PostgresPool
 create-jdbc-resource --connectionpoolid=PostgresPool jdbc/vuelosDS
 EOF
+
+echo "=== Post-boot file contents ==="
+cat /opt/payara/config/post-boot-generated.asadmin
 
 exec java \
   -jar /opt/payara/payara-micro.jar \
